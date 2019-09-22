839d7a8f6cefa5247a9a1e563f1728e4129a9744
zhanchenlong <522382456@qq.com>
2019-09-22 18:26:01 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,194 @@
+/* eslint-disable */
+
+import Vue from "vue";
+import {
+  baseStore,
+  baseRouter,
+  sysRoutes,
+  Login,
+  Main,
+  Home,
+  App,
+  baseRegComponents,
+  baseRouterView
+} from "base";
+import ElementUI from "element-ui";
+import NProgress from "nprogress";
+import moment from "moment";
+Vue.use(ElementUI);
+import axios from "axios";
+import echarts from "echarts";
+Vue.prototype.$echarts = echarts;
+Vue.prototype.axios = axios;
+Vue.prototype.$moment = moment;
+
+import VCharts from "v-charts";
+Vue.use(VCharts);
+Vue.use(baseRegComponents);
+baseStore.commit('updateProcessEnv', process.env)
+
+console.log(window);
+console.log("=============");
+console.log(process.env)
+
+// import { ccmRegComponents, ccmRoutes, ccmStore, ccmRouterView } from "ccm";
+// Vue.use(ccmRegComponents);
+// if (ccmStore) {
+//   for (var key in ccmStore) {
+//     // console.log(key);
+//     baseStore.registerModule(key, ccmStore[key]);
+//   }
+// }
+  
+console.log(baseStore);
+/**
+ * Config
+ */
+
+Vue.config.debug = process.env.NODE_ENV === "development";
+Vue.config.silent = process.env.NODE_ENV === "production";
+Vue.config.devtools = true;
+Vue.config.productionTip = false;
+
+const setGlobalTopNavs = function(to, next) {
+  let matchedLength = to.matched.length;
+  let item = to;
+  if (matchedLength > 2) {
+    item = to.matched[2];
+  }
+  if (!item.meta.hidden) {
+    let newItem = {
+      name: item.meta.title,
+      path: item.path,
+      query: item.query,
+      params: item.params
+    };
+    baseStore.commit("updateNavTabData", newItem);
+  }
+  next();
+};
+
+console.log(baseRouter);
+
+const routerView = {
+  template: "<router-view></router-view>"
+};
+
+// baseRouter.options.routes = baseRouter.options.routes.concat(router);
+
+let routes = [
+  {
+    path: "/login",
+    meta: {
+      title: "登录"
+    },
+    component: Login,
+    name: "登录"
+  },
+  {
+    path: "/",
+    meta: {
+      title: "首页"
+    },
+    component: Main,
+    redirect: "home",
+    children: [
+      // 首页
+      {
+        path: "home",
+        meta: {
+          title: "工作台",
+          hidden: true
+        },
+        component: Home,
+        name: "主页"
+      },
+      //基础平台系统路由
+      {
+        path: "sys",
+        meta: {
+          title: "系统管理"
+        },
+        component: baseRouterView,
+        name: "系统管理",
+        children: sysRoutes
+      },
+
+      // 票券系统路由
+      // {
+      //   path: "coupon",
+      //   meta: {
+      //     title: "票券系统"
+      //   },
+      //   component: ccmRouterView,
+      //   name: "票券系统",
+      //   children: ccmRoutes
+      // },
+    ]
+  }
+];
+baseRouter.options.routes = routes;
+baseRouter.addRoutes(routes);
+
+baseRouter.beforeEach((to, from, next) => {
+  let token = sessionStorage.getItem("token");
+  let user = localStorage.getItem("user");
+  NProgress.start();
+  /*if(from.path=='/login'){
+      sessionStorage.removeItem('tagNavList')
+  }*/
+  if (to.path == "/login") {
+    localStorage.clear();
+    baseStore.commit("updateLoginToken", null);
+    baseStore.commit("updateLoginUser", null);
+    baseStore.commit("updateUserMenu", null);
+    // store.commit('updateNavTabDataByIndex',[{
+    //     path: '/home',
+    //     title: '主页',
+    // }] )
+
+    next();
+  } else {
+    if (!baseStore.state.loginToken || !user) {
+      if (token && user) {
+        baseStore.commit("updateLoginTokenCPM", token);
+        baseStore.commit("updateLoginUserCPM", JSON.parse(user));
+        setGlobalTopNavs(to, next);
+      } else {
+        next({
+          path: "/login"
+        });
+      }
+    } else {
+      setGlobalTopNavs(to, next);
+    }
+  }
+  next();
+});
+baseRouter.afterEach(transition => {
+  NProgress.done();
+  // setTimeout(() => {
+  //     if (document && document.getElementById('wholeLoadingId') && document.getElementById('wholeLoadingId').style.display != 'none') {
+  //         document.getElementById('wholeLoadingId').style.display = 'none'
+  //     }
+  // }, 20)
+});
+
+//  全局组件之间通信
+Vue.prototype.$eventHub = Vue.prototype.$eventHub || new Vue();
+
+// 设置浏览器窗口大小
+window.onresize = function(e) {
+  baseStore.commit("changeWindowSize", {
+    innerWidth: e.target.innerWidth,
+    innerHeight: e.target.innerHeight
+  });
+};
+
+new Vue({
+  router: baseRouter,
+  store: baseStore,
+  render: h => h(App)
+}).$mount("#app");
+
+/* eslint-disable */
